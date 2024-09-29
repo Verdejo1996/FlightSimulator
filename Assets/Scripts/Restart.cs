ed05306f656678c45ddfec766d15bfdb4f32e5a4
Verdejo1996 <verdejo1996@gmail.com>
2024-09-29 19:31:44 -0300

    Second commit

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(RestartAfterDelay(1.0f)); // Espera 1 segundo antes de reiniciar
+        }
+    }
+
+    IEnumerator RestartAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
