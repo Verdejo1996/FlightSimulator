e1ab78095db4ca465787dfa074713c326c9d1c5c
Verdejo1996 <verdejo1996@gmail.com>
2024-11-11 18:20:46 -0300

    Another corrections

--- Assets/Scripts/Hud_Controller.cs
+++ Assets/Scripts/Hud_Controller.cs
@@ -18,7 +18,7 @@ public class Hud_Controller : MonoBehaviour
     public Text finalTimeText; // Texto para el tiempo total
     public Text averageHeightText; // Texto para la altura promedio
     public Text finalScoreText; // Texto para el puntaje final
-    //public Text distanceTarget; // Texto para la distancia al objetivo
+    public Text distanceTarget; // Texto para la distancia al objetivo
     public Text endTutorial; // Texto para la distancia al objetivo
 
     private float averageHeight;
@@ -66,9 +66,9 @@ public class Hud_Controller : MonoBehaviour
 
         timer.text = string.Format("Time: {0:00}:{1:00}", minutes, seconds);
 
-        /*//Calcula la distancia entre el avion y el objetivo
+        //Calcula la distancia entre el avion y el objetivo
         float distance = Vector3.Distance(target.position, airplane.position);
-        distanceTarget.text = "Distancia hacia el objetivo: " + distance.ToString("F2") + " metros";*/
+        distanceTarget.text = "Distancia hacia el objetivo: " + distance.ToString("F2") + " metros";
     }
 
     // Método para sumar puntos al puntaje
